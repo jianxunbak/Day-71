fe3ea299014b24987e55703a8137fa8a88c1f971
jianxun <jianxunbak@gmail.com>
2024-07-08 20:57:51 +0800

    updated environment data

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL', 'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
